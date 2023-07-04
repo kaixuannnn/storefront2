cf04418b0aa82c62c488e9a758e495cf7fd818b0
kaixuan <kaixuant@yahoo.com>
2023-07-04 21:50:20 +0800

    retrieve objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-
-    for product in query_set:
-        print(product)
+    # We can do this way to retrieve object, however we need to do error handling, therefore
+    # try: 
+    #     product = Product.objects.get(pk=0)
+    # except ObjectDoesNotExist:
+    #     pass
+    # We can use filter, as it return querySet and when we ask for first, it will return null
+    # product = Product.objects.filter(pk=0).first()
+    # we also can check the existence of the object
+    exists = Product.objects.filter(pk=0).exists()
 
     return render(request, 'hello.html', {'name': 'Mosh'})
