6661cd7ff483006786e94ee7c6ac02625af22087
kaixuan <kaixuant@yahoo.com>
2023-07-05 19:48:04 +0800

    aggregate

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.db.models import Q, F
-from store.models import Product, OrderItem, Order
-
+from store.models import Product
+from django.db.models import Count
 
 def say_hello(request):
-   # Exercise: We want to get the Order query_set to access the associated customer and orderitem and product for the first 5 items
-   query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-   return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
+   result = Product.objects.aggregate(count=Count('id'))
+   return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
