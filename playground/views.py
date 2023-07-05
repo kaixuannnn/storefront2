45e68894d4d40d0483d8bd072afb9b3d551397b4
kaixuan <kaixuant@yahoo.com>
2023-07-05 16:51:02 +0800

    prefetch_related & select_related exercise

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 
 def say_hello(request):
-   query_set = Product.objects.prefetch_related('promotions').all()
+   # Exercise: We want to get the Order query_set to access the associated customer and orderitem and product for the first 5 items
+   query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
