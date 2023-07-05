2506652d9e87e3dc378af8739abbe89fda89e927
kaixuan <kaixuant@yahoo.com>
2023-07-05 16:41:38 +0800

    select_related & prefetch_related

--- playground/views.py
+++ playground/views.py
@@ -4,7 +4,5 @@ from store.models import Product, OrderItem
 
 
 def say_hello(request):
-   # Exersice: We want to find the list of product which had been ordered and sort by title
-   # distinct() - to eliminate redundant record
-   query_set =  Product.objects.filter(id__in = OrderItem.objects.values_list('product__id').distinct()).order_by('title')
+   query_set = Product.objects.prefetch_related('promotions').all()
    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
