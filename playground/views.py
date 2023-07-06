5f6ae15cd226d777ea9a5e8a38737535e9703977
kaixuan <kaixuant@yahoo.com>
2023-07-06 15:56:00 +0800

    query generic relationship

--- playground/views.py
+++ playground/views.py
@@ -2,8 +2,17 @@ from django.forms import DecimalField
 from django.shortcuts import render
 from store.models import Customer, Product
 from django.db.models import Value, F, Count, ExpressionWrapper, DecimalField
+from django.contrib.contenttypes.models import ContentType
+
+from tags.models import TaggedItem
 
 def say_hello(request):
-   discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
-   queryset = Product.objects.annotate(discounted_price=discounted_price)
+   content_type = ContentType.objects.get_for_model(Product)
+
+   queryset = TaggedItem.objects\
+        .select_related('tag')\
+        .filter(
+            content_type=content_type,
+            object_id=1
+        )
    return render(request, 'hello.html', {'name': 'Mosh', 'products': queryset})
