72e725dbe5ab61c8d4af28546f102cb87cda730d
kaixuan <kaixuant@yahoo.com>
2023-07-06 10:39:04 +0800

    Expression Wrapper

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,9 @@
+from django.forms import DecimalField
 from django.shortcuts import render
-from store.models import Customer
-from django.db.models import Value, F, Count
+from store.models import Customer, Product
+from django.db.models import Value, F, Count, ExpressionWrapper, DecimalField
 
 def say_hello(request):
-   queryset = Customer.objects.annotate(order_count=Count('order__id'))
+   discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
+   queryset = Product.objects.annotate(discounted_price=discounted_price)
    return render(request, 'hello.html', {'name': 'Mosh', 'products': queryset})
