164ef358a660688b2ba25cbb2a09ad010728b0d7
kaixuan <kaixuant@yahoo.com>
2023-07-08 12:18:53 +0800

    create & update objects

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,14 @@
 from django.forms import DecimalField
 from django.shortcuts import render
-from store.models import Customer, Product
+from store.models import Customer, Product, Collection
 from django.db.models import Value, F, Count, ExpressionWrapper, DecimalField
 from django.contrib.contenttypes.models import ContentType
 
 from tags.models import TaggedItem
 
 def say_hello(request):
-   content_type = ContentType.objects.get_for_model(Product)
-
-   queryset = TaggedItem.objects\
-        .get_tags_for(Product, 1)
+   collection = Collection()
+   collection.name = 'Video Games'
+   collection.featured_product = Product(pk=1)
+   collection.save()
    return render(request, 'hello.html', {'name': 'Mosh', 'products': queryset})
