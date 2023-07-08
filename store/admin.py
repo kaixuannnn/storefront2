ec2748412542bb483bbb2851c71594ac8cc8b65b
kaixuan <kaixuant@yahoo.com>
2023-07-08 22:20:39 +0800

    Searching

--- store/admin.py
+++ store/admin.py
@@ -46,6 +46,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders(self, customer):
