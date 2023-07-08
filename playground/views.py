81ffb3757f08688c710d2b8f3dcae184294d1ff8
kaixuan <kaixuant@yahoo.com>
2023-07-08 15:20:30 +0800

    update

--- playground/views.py
+++ playground/views.py
@@ -7,8 +7,4 @@ from django.contrib.contenttypes.models import ContentType
 from tags.models import TaggedItem
 
 def say_hello(request):
-   collection = Collection()
-   collection.name = 'Video Games'
-   collection.featured_product = Product(pk=1)
-   collection.save()
-   return render(request, 'hello.html', {'name': 'Mosh', 'products': queryset})
+   return render(request, 'hello.html', {'name': 'Mosh'})
