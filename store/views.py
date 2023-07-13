eba71ee2689089ce163f12c5374f78ab528d9d7c
kaixuan <kaixuant@yahoo.com>
2023-07-13 15:43:03 +0800

    saving objects & updating objects

--- store/views.py
+++ store/views.py
@@ -2,23 +2,43 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from store.models import Product
 from store.serializers import ProductSerializer
 from django.shortcuts import get_object_or_404
 
 
-@api_view()
+@api_view(['GET','POST'])
 def product_list(request):
-    query_set = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(query_set, many=True,  context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        query_set = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(query_set, many=True,  context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        # if serializer.is_valid():
+        # used to read the validated_data inside the serializer
+        #     serializer.validated_data
+        #     return Response('ok')
+        # else:
+        #     return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+        # We can do it another way
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view()
+@api_view(['GET', 'PUT', 'PATCH'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
 @api_view()
 def collection_detail(request, pk):
