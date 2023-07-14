8185269e5257d4ea63e0adcabf5d76666dd3bc99
kaixuan <kaixuant@yahoo.com>
2023-07-14 12:26:53 +0800

    Router

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 from . import views
+from pprint import pprint
 
-# URLConf
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+pprint(router.urls)
+
 urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection_detail')
+     path('', include(router.urls))
 ]
