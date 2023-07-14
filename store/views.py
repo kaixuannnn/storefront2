ac57598931efc57be014d3c60a5086d0f91c677c
kaixuan <kaixuant@yahoo.com>
2023-07-14 12:03:58 +0800

    ViewSet

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from store.models import Product, Collection
@@ -11,25 +12,20 @@ from store.serializers import CollectionSerializer, ProductSerializer
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
 
-class ProductList(ListCreateAPIView):
-    # for simple queryset, and serializer we just defined it in a field
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializer
-    
-    
+
     def get_serializer_context(self):
         return {'request': self.request}
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
+    
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if product.orderitems.count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
     
 class CollectionList(ListCreateAPIView):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
