34654f7fccfec62f5fb05d2a0fa5bac31ed453da
kaixuan <kaixuant@yahoo.com>
2023-07-14 20:36:41 +0800

    Creating a cart

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from decimal import Decimal
 
-from store.models import Collection, Product, Review
+from store.models import Cart, Collection, Product, Review
 from django.db.models import Count
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -32,3 +32,9 @@ class ReviewSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         product_id = self.context['product_id']
         return Review.objects.create(product_id=product_id, **validated_data)
+
+class CartSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    class Meta:
+        model = Cart
+        fields = ['id']
--- store/views.py
+++ store/views.py
@@ -1,13 +1,14 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from store.filters import ProductFilter
-from store.models import OrderItem, Product, Collection, Review
+from store.models import Cart, OrderItem, Product, Collection, Review
 from store.pagination import DefaultPagination
-from store.serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from store.serializers import CartSerializer, CollectionSerializer, ProductSerializer, ReviewSerializer
 from django.db.models import Count
 
 class ProductViewSet(ModelViewSet):
@@ -47,7 +48,10 @@ class ReviewViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
- 
+    
+class CartViewSet(CreateModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
 
 
    
