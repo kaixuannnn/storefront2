6fd5356b4b196c1ba83626846186b4c6f24ca5a1
kaixuan <kaixuant@yahoo.com>
2023-07-19 13:29:30 +0800

    applying permissions

--- store/views.py
+++ store/views.py
@@ -105,5 +105,12 @@ class CustomerViewSet(ModelViewSet):
         return Response('ok')
 
 class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.prefetch_related('items__product').all()
     serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Order.objects.all()
+        
+        (customer_id, created) = Customer.objects.only('id').get_or_create(user_id=user.id)
+        return Order.objects.filter(customer_id=customer_id)
